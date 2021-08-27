7d54e26beace5314295b6427f2ba23859391101e
seoljihee <wlgml7997@naver.com>
2021-08-27 22:20:32 +0900

    case 2 완료

---
+++ src/main/Interface2.java
@@ -0,0 +1,6 @@
+package main;
+
+public interface Interface2 {
+
+	public void memberView();
+}
---
+++ src/main/Jihee.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Jihee implements Interface2{
+
+	@Override
+	public void memberView() {
+		System.out.println("회원정보를 출력합니다.");
+	}
+
+	
+}
