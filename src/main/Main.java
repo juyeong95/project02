09f02833c06b92b3680112fd2a26023eaea98da7
kwanghun <s5095940@naver.com>
2021-08-27 21:49:17 +0900

    회원탈퇴 등록

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		Maininter m = new Memberout();
 		while(true) {
 			System.out.println("기능을 선택하세요");
 			System.out.println("1.회원가입 2.회원보기 3.회원탈퇴 4.이벤트등록 5.이벤트보기 6.종료");
@@ -14,7 +15,8 @@ public class Main {
 			switch(num) {
 			case 1: break;
 			case 2: break;
-			case 3: break;
+			case 3: m.memout();
+				break;
 			case 4: break;
 			case 5: break;
 			case 6: return;
